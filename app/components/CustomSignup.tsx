934e5e7dc568535a05c75c040fdb166df03c3bbc
Pranai-1 <reddypranai2017@gmail.com>
2025-02-18 10:13:57 +0530

    prisma migration

--- app/components/CustomSignup.tsx
+++ app/components/CustomSignup.tsx
@@ -8,15 +8,17 @@ export default function CustomSignup() {
 
     return (
         <form action={loginAction}>
-            <div className="flex flex-col justify-start items-start gap-3">
-                <div className="flex flex-col gap-2 w-full">
+            <div className="flex flex-col justify-start items-start gap-3 text-sm sm:text-base">
+                <div className="flex flex-col gap-2 w-full ">
                     <label htmlFor="firstName">First name *</label>
                     <input
                         name="firstName"
                         id="firstName"
                         type="text"
                         placeholder="Your first name"
-                        className="p-2 border-2 border-[#B7BDC2] hover:border-[#5B89EF]"
+                        minLength={2} // Minimum of 2 characters
+                        maxLength={3} // Maximum of 30 characters 
+                        className="p-1 sm:p-2 border-2 border-[#B7BDC2] hover:border-[#5B89EF]"
                     />
                 </div>
 
@@ -42,12 +44,12 @@ export default function CustomSignup() {
                     />
                 </div>
 
-                <button
-                    className="bg-[#5D5DFF] w-full rounded-lg font-medium text-center text-white p-2 py-4 mt-3 text-lg"
+                <input
+                    className="bg-[#5D5DFF] cursor-pointer w-full rounded-lg font-medium text-center text-white p-2 sm:py-4 mt-3 text-lg"
                     type="submit"
-                >
-                    Signup
-                </button>
+                />
+                   
+               
             </div>
         </form>
     );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -9,10 +9,10 @@ export default function Signup(){
 
    
     return(
-        <div className="p-3 bg-black flex justify-center h-screen flex-wrap">
+        <div className="p-3 bg-black flex justify-center items-center h-screen flex-wrap">
            
-            <div className="w-[250px] sm:w-[450px] h-max mt-10 bg-[#FFFFFF] p-5 rounded-lg px-10">
-                <p className="text-xl m-5 text-center font-medium">Register with your email</p>
+            <div className="w-[95%] sm:w-[450px] h-max mt-10 bg-[#FFFFFF] p-2 sm:p-5 rounded-lg sm:px-10">
+                <p className="sm:text-xl m-5 text-center font-medium">Register with your email</p>
              <CustomSignup/>
                 <div className="mt-6 w-[100%] flex flex-col gap-3 items-center justify-center">
                         <p className="text-center font-medium">OR</p>
